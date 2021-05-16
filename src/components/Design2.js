81fa823eabcdd826925e377c0713cd2358352818
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-16 09:27:45 +0100

    add bottom design component

--- src/components/Design2.js
+++ src/components/Design2.js
@@ -11,6 +11,7 @@ function Design2() {
                     <h3>Free, Open, Simple</h3>
                     <p>Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
                 </div>
+                <br />
                 <div className="dright-2">
                     <h3>Powerful tooling</h3>
                     <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
