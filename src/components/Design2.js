0173a1f312dbf48123481d510243e02f5dd0ccfc
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-18 14:30:26 +0100

    improve responsiveness for middle and design2 component

--- src/components/Design2.js
+++ src/components/Design2.js
@@ -8,13 +8,13 @@ function Design2() {
             <img src={ laptopdesk } className="dleft" alt="" />
             <div className="dright">
                 <div className="dright-1">
-                    <h3>Free, Open, Simple</h3>
-                    <p>Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
+                    <h3 className="dsm1">Free, Open, Simple</h3>
+                    <p className="dsm2">Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
                 </div>
                 <br />
                 <div className="dright-2">
-                    <h3>Powerful tooling</h3>
-                    <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
+                    <h3 className="dsm1">Powerful tooling</h3>
+                    <p className="dsm2">Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
                 </div>
             </div>
         </div>
--- src/components/Middle.js
+++ src/components/Middle.js
@@ -6,7 +6,7 @@ function Middle() {
     return (
         <div className="middle-curve">
             <div className="mc">
-                <img src={ phones } className="phones" alt="" style={{ width: '40%', marginLeft: '50px' }} />
+                <img src={ phones } className="phones" alt="" />
                 <div className="mcright">
                     <h3 className="rsm">State of The Art infrastucture</h3>
                     <p className="rsm2">With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
